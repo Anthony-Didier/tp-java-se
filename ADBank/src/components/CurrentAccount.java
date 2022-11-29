a1c8ed66955f15563c388aae786cafd007e45ded
Anthony-Didier <didieranthony1@gmail.com>
2022-11-29 11:21:33 +0100

    CurrentAccount and SavingsAccount classes created

---
+++ ADBank/src/components/CurrentAccount.java
@@ -0,0 +1,12 @@
+// 1.2.2 Creation of the CurrentAccount and SavingsAccount
+
+package components;
+
+public class CurrentAccount extends Account {
+
+	public CurrentAccount(String label, float balance, int accountNumber, Client clientNumber) {
+		super(label, balance, accountNumber, clientNumber);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ADBank/src/components/SavingsAccount.java
@@ -0,0 +1,12 @@
+// 1.2.2 Creation of the CurrentAccount and SavingsAccount
+
+package components;
+
+public class SavingsAccount extends Account {
+
+	public SavingsAccount(String label, float balance, int accountNumber, Client clientNumber) {
+		super(label, balance, accountNumber, clientNumber);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
